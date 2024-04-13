287a1b93cfcddca2f8b471e875aeb0bcfe38f832
berkaysson <berkaysonel85@gmail.com>
2024-04-13 13:13:41 +0300

    Configure Routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <main>
+        <nav />
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </section>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
