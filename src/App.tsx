74b487e57673251edd6db986f7f33ac4785b83bd
berkaysson <berkaysonel85@gmail.com>
2024-04-13 16:04:28 +0300

    Implement public and private routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,49 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
+import PublicRoute from "./components/routes/PublicRoutes";
+import PrivateRoute from "./components/routes/PrivateRoutes";
+import { ProfileProvider } from "./context/profile.context";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main>
-        <nav />
-        <section>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </section>
-      </main>
-    </BrowserRouter>
+    <div>
+      <ProfileProvider>
+        <BrowserRouter>
+          <main>
+            <nav />
+            <section>
+              <Routes>
+                <Route
+                  path="/login"
+                  element={
+                    <PublicRoute>
+                      <Login />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/register"
+                  element={
+                    <PublicRoute>
+                      <Register />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/"
+                  element={
+                    <PrivateRoute>
+                      <Home />
+                    </PrivateRoute>
+                  }
+                />
+              </Routes>
+            </section>
+          </main>
+        </BrowserRouter>
+      </ProfileProvider>
+    </div>
   );
 }
 
