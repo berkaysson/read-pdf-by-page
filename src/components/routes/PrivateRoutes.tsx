10d0c72628290f7ace71d3b4aefd955b20303b6d
berkaysson <berkaysonel85@gmail.com>
2024-04-13 16:04:16 +0300

    Add private and public route to manage authentication across pages

---
+++ src/components/routes/PrivateRoutes.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProfileContext } from "../../context/profile.context";
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const { profile, isLoading } = useContext(ProfileContext);
+
+  const navigate = useNavigate();
+
+  // Effect to redirect unauthenticated users to the signin route
+  useEffect(() => {
+    // If data is loaded and no user profile exists (user is not authenticated)
+    if (!isLoading && !profile) {
+      // Redirect unauthenticated users to the signin route ("/signin")
+      navigate("/login");
+    }
+  }, [isLoading, profile, navigate]); // Dependency array to trigger the effect
+
+  // Conditional rendering based on loading state
+  return <>{isLoading ? <p>Loading...</p> : children}</>;
+};
+
+export default PrivateRoute;
---
+++ src/components/routes/PublicRoutes.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProfileContext } from "../../context/profile.context";
+
+interface PublicRouteProps {
+  children: ReactNode;
+}
+
+const PublicRoute: React.FC<PublicRouteProps> = ({ children }) => {
+  // Accessing profile and isLoading state from the ProfileContext
+  const { profile, isLoading } = useContext(ProfileContext);
+
+  // Hook from React Router DOM to navigate between routes
+  const navigate = useNavigate();
+
+  // Effect to redirect authenticated users away from public routes
+  useEffect(() => {
+    // If data is loaded and a user profile exists (user is authenticated)
+    if (!isLoading && profile) {
+      // Redirect authenticated users to the home route ("/")
+      navigate("/");
+    }
+  }, [isLoading, profile, navigate]); // Dependency array to trigger the effect
+
+  // Conditional rendering based on loading state
+  return <>{isLoading ? <p>Loading...</p> : children}</>;
+};
+
+export default PublicRoute;
