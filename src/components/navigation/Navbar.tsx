f62e6bc0389ba2259bbda81760c2e8e234f40df7
berkaysson <berkaysonel85@gmail.com>
2024-04-13 16:35:08 +0300

    Add log out button to navigation

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { logout } from '../../utilities/signOutUtilities'
+import { ProfileContext } from '../../context/profile.context'
 
 export const Navbar = () => {
+  const { profile } = useContext(ProfileContext);
   return (
-    <div>Nav</div>
+    <nav>
+      {profile && <button onClick={logout}>Log Out</button>}
+    </nav>
   )
 }
