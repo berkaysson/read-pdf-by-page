fbbe9cd32a634dea4380b52d53f91248c9f7181f
berkaysson <berkaysonel85@gmail.com>
2024-04-14 18:53:56 +0300

    Add update pdf savedPage utility

--- src/utilities/pdfUtilities.ts
+++ src/utilities/pdfUtilities.ts
@@ -20,7 +20,30 @@ export const addOrUpdateSavedPdf = (
 
     return updatedProfile;
   }
-  
+
   // If PDF with the same title already exists, return the original profile
+  return profile;
+};
+
+export const updateSavedPdfSavedPage = (
+  pdfTitle: string,
+  newPdfPage: number,
+  database: Database,
+  profile: UserProfile
+) => {
+  const existingPdfIndex = profile.savedPdfs.findIndex(
+    (pdf) => pdf.title === pdfTitle
+  );
+
+  if (existingPdfIndex >= 0) {
+    const updatedPdfs = [...profile.savedPdfs];
+    updatedPdfs[existingPdfIndex].savedPage = newPdfPage;
+    const updatedProfile = { ...profile, savedPdfs: updatedPdfs };
+    set(ref(database, `users/${profile.uid}`), updatedProfile);
+
+    return updatedProfile;
+  } else {
+    alert("Pdf is not existing in database.");
+  }
   return profile;
 };
