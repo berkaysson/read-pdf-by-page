58b8fe7d61986f387af8acd395ba2fbdbb54597b
berkaysson <berkaysonel85@gmail.com>
2024-04-14 18:56:36 +0300

    Create PDFDislpay to render retrieved text from PDF

--- src/components/main/displays/PDFDisplay.tsx
+++ src/components/main/displays/PDFDisplay.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { PDFContext } from "../../../context/pdf.context";
 
 export const PDFDisplay: React.FC = () => {
+  const [pages, setPages] = useState<string[]>([]);
+  const { activePDFContent } = useContext(PDFContext);
+
+  useEffect(() => {
+    if (activePDFContent) {
+      setPages([...activePDFContent]);
+    } else {
+      setPages([]);
+    }
+  }, [activePDFContent]);
+
   return (
-    <div>PDFDisplay</div>
-  )
-}
+    <div>
+      {pages.map((page, index) => (
+        <>
+          <p key={index} id={"page-" + (index + 1)}>
+            {page}
+          </p>
+          <br key={"break"+index} />
+        </>
+      ))}
+    </div>
+  );
+};
