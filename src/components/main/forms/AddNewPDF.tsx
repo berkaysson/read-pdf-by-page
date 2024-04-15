c3922a8b952145d0e27c9e2456e2df9edaf0bfc4
berkaysson <berkaysonel85@gmail.com>
2024-04-16 01:30:31 +0300

    Add box shadows

--- src/components/main/forms/AddNewPDF.tsx
+++ src/components/main/forms/AddNewPDF.tsx
@@ -46,7 +46,7 @@ export const AddNewPDF = () => {
   return (
     <div className="flex flex-col gap-2">
       <label
-        className="items-center justify-center p-4 text-5xl text-center align-middle rounded-md cursor-pointer bg-secondary text-primary hover:opacity-90"
+        className="items-center shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] justify-center p-4 text-5xl text-center align-middle rounded-md cursor-pointer bg-secondary text-primary hover:opacity-90"
         htmlFor="fileInput"
       >
         {isFileLoading ? "..." : "+"}
