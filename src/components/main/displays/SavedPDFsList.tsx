95623023ac5c997b565731c8b59d5a01880dc8cd
berkaysson <berkaysonel85@gmail.com>
2024-04-16 01:13:36 +0300

    Implement sort algorithm for saved pdfs

--- src/components/main/displays/SavedPDFsList.tsx
+++ src/components/main/displays/SavedPDFsList.tsx
@@ -4,6 +4,13 @@ import { ProfileContext } from "../../../context/profile.context";
 export const SavedPDFsList: React.FC = () => {
   const { profile } = useContext(ProfileContext);
   const savedPdfsList = profile?.savedPdfs;
+  if (savedPdfsList) {
+    savedPdfsList.sort(
+      (a, b) =>
+        new Date(b.updateDate).getTime() - new Date(a.updateDate).getTime()
+    );
+  }
+
   return (
     <ul className="flex flex-col gap-2">
       {savedPdfsList?.map((pdfItem, index) => (
@@ -11,7 +18,9 @@ export const SavedPDFsList: React.FC = () => {
           className="p-2 rounded-md shadow-sm bg-secondary"
           key={"pdfItem" + index}
         >
-          <p className="pb-4 overflow-x-auto whitespace-nowrap">{pdfItem.title}</p>
+          <p className="pb-4 overflow-x-auto whitespace-nowrap">
+            {pdfItem.title}
+          </p>
           Page:
           <span className="font-bold"> {pdfItem.savedPage}</span>
         </li>
