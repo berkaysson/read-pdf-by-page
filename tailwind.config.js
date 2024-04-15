e5e4397547470db426b0860abd271ff46a241a79
berkaysson <berkaysonel85@gmail.com>
2024-04-16 00:46:50 +0300

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2B2D42",
+        secondary: "#8D99AE",
+        light: "#EDF2F4",
+        accent: "#EF233C",
+      },
+    },
   },
   plugins: [],
 };
