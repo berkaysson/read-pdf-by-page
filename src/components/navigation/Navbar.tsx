8fba4a20d8f07b21c5860348dccd56df5993f357
berkaysson <berkaysonel85@gmail.com>
2024-04-16 01:28:09 +0300

    Add gap

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar: React.FC<{ isNavOpen: boolean }> = ({ isNavOpen }) => {
   } h-screen duration-300 z-50 fixed top-0 w-3/4 md:w-1/2 flex-shrink-0 bg-light text-primary`;
   return (
     <nav className={navClass}>
-      <div className="flex shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] flex-col justify-between h-full px-4 py-4 overflow-x-hidden overflow-y-auto align-middle">
+      <div className="flex shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] flex-col justify-between gap-4 h-full px-4 py-4 overflow-x-hidden overflow-y-auto align-middle">
         <Link to="/" className="text-3xl font-bold md:text-5xl">
           RPBP
         </Link>
