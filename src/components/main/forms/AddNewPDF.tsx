b9f2ab9ebcc7be7087be471fcd24f7a2283593c2
berkaysson <berkaysonel85@gmail.com>
2024-05-11 22:52:04 +0300

    Implement setNewPdf and isLaoding from Pdf Context

--- src/components/main/forms/AddNewPDF.tsx
+++ src/components/main/forms/AddNewPDF.tsx
@@ -1,17 +1,22 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ProfileContext } from "../../../context/profile.context";
 import { SavedPdf } from "../../../context/profile.types";
-import pdfToText from "../../../utilities/pdfToText.js";
 import { PDFContext } from "../../../context/pdf.context";
 import { FcAddDatabase } from "react-icons/fc";
 
 export const AddNewPDF = () => {
   const { handleAddSavedPdf } = useContext(ProfileContext);
-  const { activePDFTitle, setActivePDFContent, setActivePDFTitle } =
-    useContext(PDFContext);
+  const {
+    activePDFTitle,
+    setNewPDF,
+    isFileLoading,
+    progress,
+    setProgress,
+    setIsFileLoading,
+    setFileLoadingType,
+    fileLoadingType,
+  } = useContext(PDFContext);
   const [selectedPdf, setSelectedPdf] = useState<File | null>(null);
-  const [isFileLoading, setIsFileLoading] = useState<boolean>(false);
-  const [progress, setProgress] = useState(100);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -20,29 +25,25 @@ export const AddNewPDF = () => {
     }
   };
 
-  const handleSave = () => {
+  const handleSave = async () => {
     if (selectedPdf) {
+      setIsFileLoading(true);
+      setFileLoadingType("Uploading...");
       const newPdf: SavedPdf = {
         title: selectedPdf.name,
         savedPage: 0,
         updateDate: new Date().toISOString(),
         downloadURL: "",
       };
-
-      handleAddSavedPdf(newPdf, selectedPdf);
+      await handleAddSavedPdf(newPdf, selectedPdf, setProgress);
+      setProgress(100);
+      setIsFileLoading(false);
     }
   };
 
   useEffect(() => {
     if (selectedPdf) {
-      setIsFileLoading(true);
-      pdfToText(selectedPdf, setProgress)
-        .then((pdf) => {
-          setActivePDFContent(pdf);
-          setActivePDFTitle(selectedPdf.name);
-          setIsFileLoading(false);
-        })
-        .catch((error) => console.error("Failed to extract text from pdf"));
+      setNewPDF(selectedPdf);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedPdf]);
@@ -54,11 +55,15 @@ export const AddNewPDF = () => {
           className="items-center shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] justify-center p-4 text-5xl text-center align-middle rounded-md cursor-pointer bg-secondary text-primary hover:opacity-90"
           htmlFor="fileInput"
         >
-          {isFileLoading ? "..." : "+"}
+          {isFileLoading ? (
+            <span className="text-sm">{fileLoadingType}</span>
+          ) : (
+            "+"
+          )}
           {
             <div
               className="p-2 mt-2 text-xs text-left border-solid rounded text-secondary border-light bg-primary opacity-80"
-              style={{ width: `${progress}%` }}
+              style={{ width: `${isFileLoading ? progress : 100}%` }}
             >
               {isFileLoading ? progress.toFixed(0) + "%" : "Add new PDF"}
             </div>
