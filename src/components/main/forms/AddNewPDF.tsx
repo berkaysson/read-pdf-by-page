4c3343ba69424dc1eed2a04f9446727f7bc762c0
berkaysson <berkaysonel85@gmail.com>
2024-05-11 17:37:57 +0300

    Add form wrapper to AddNewPdf

--- src/components/main/forms/AddNewPDF.tsx
+++ src/components/main/forms/AddNewPDF.tsx
@@ -7,7 +7,8 @@ import { FcAddDatabase } from "react-icons/fc";
 
 export const AddNewPDF = () => {
   const { handleAddSavedPdf } = useContext(ProfileContext);
-  const { activePDFTitle,setActivePDFContent, setActivePDFTitle } = useContext(PDFContext);
+  const { activePDFTitle, setActivePDFContent, setActivePDFTitle } =
+    useContext(PDFContext);
   const [selectedPdf, setSelectedPdf] = useState<File | null>(null);
   const [isFileLoading, setIsFileLoading] = useState<boolean>(false);
   const [progress, setProgress] = useState(100);
@@ -47,35 +48,38 @@ export const AddNewPDF = () => {
 
   return (
     <div className="flex flex-col gap-2">
-      <label
-        className="items-center shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] justify-center p-4 text-5xl text-center align-middle rounded-md cursor-pointer bg-secondary text-primary hover:opacity-90"
-        htmlFor="fileInput"
-      >
-        {isFileLoading ? "..." : "+"}
-        {
-          <div
-            className="p-2 mt-2 text-xs text-left border-solid rounded text-secondary border-light bg-primary opacity-80"
-            style={{ width: `${progress}%` }}
-          >
-            {isFileLoading ? progress.toFixed(0) + "%" : "Add new PDF"}
-          </div>
-        }
-      </label>
-      <input
-        type="file"
-        id="fileInput"
-        accept=".pdf"
-        onChange={handleFileChange}
-        disabled={isFileLoading}
-      />
-      <button
-        className="flex items-center justify-center gap-2 btn"
-        onClick={handleSave}
-        disabled={activePDFTitle === "" || !activePDFTitle}
-      >
-        <FcAddDatabase className="text-lg" />
-        <span>Save PDF</span>
-      </button>
+      <form className="flex flex-col gap-2">
+        <label
+          className="items-center shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] justify-center p-4 text-5xl text-center align-middle rounded-md cursor-pointer bg-secondary text-primary hover:opacity-90"
+          htmlFor="fileInput"
+        >
+          {isFileLoading ? "..." : "+"}
+          {
+            <div
+              className="p-2 mt-2 text-xs text-left border-solid rounded text-secondary border-light bg-primary opacity-80"
+              style={{ width: `${progress}%` }}
+            >
+              {isFileLoading ? progress.toFixed(0) + "%" : "Add new PDF"}
+            </div>
+          }
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          accept=".pdf"
+          onChange={handleFileChange}
+          disabled={isFileLoading}
+        />
+        <button
+          className="flex items-center justify-center gap-2 btn"
+          onClick={handleSave}
+          disabled={activePDFTitle === "" || !activePDFTitle}
+          type="button"
+        >
+          <FcAddDatabase className="text-lg" />
+          <span>Save PDF</span>
+        </button>
+      </form>
     </div>
   );
 };
