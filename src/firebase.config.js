3d88df4dbf0b54e58cecab645231dbc4d87943cf
berkaysson <berkaysonel85@gmail.com>
2024-05-11 18:45:37 +0300

    Configure Firebase Cloud Storage

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
@@ -19,3 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
+export const storage = getStorage(app);
