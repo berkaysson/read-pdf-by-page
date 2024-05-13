53e284fc296a5065aa7c6537e19cfaf625e6b17f
berkaysson <berkaysonel85@gmail.com>
2024-05-13 18:56:12 +0300

    Add null pointer and useEffect with clearout

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import { ProfileProvider } from "./context/profile.context";
 import { Layout } from "./components/routes/Layout";
 import { Navbar } from "./components/navigation/Navbar";
 import { PDFProvider } from "./context/pdf.context";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
-  const navRef = useRef<HTMLElement>(null);
+  const navRef = useRef<HTMLElement | null>(null);
 
   const handleOutsideClick = (event: MouseEvent) => {
     if (isNavOpen && !navRef.current?.contains(event.target as Node)) {
       setIsNavOpen(false);
     }
   };
-  document.addEventListener("click", handleOutsideClick);
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
   return (
     <div className="h-screen bg-secondary">
       <ProfileProvider>
@@ -30,10 +37,10 @@ function App() {
               </div>
               <section className="w-full h-full p-6">
                 <h2 className="mt-8 text-xl">
-                  <span className="text-2xl font-bold">RPBP</span> (Read PDF By Page) is a web application that allows users
-                  to read PDF files by page. With RPBP, users can easily upload
-                  PDF files, navigate through pages, and view PDF content
-                  conveniently.
+                  <span className="text-2xl font-bold">RPBP</span> (Read PDF By
+                  Page) is a web application that allows users to read PDF files
+                  by page. With RPBP, users can easily upload PDF files,
+                  navigate through pages, and view PDF content conveniently.
                 </h2>
                 <Layout />
               </section>
