76f65eb460a966b87bd2813c4a242c7a4eee1037
berkaysson <berkaysonel85@gmail.com>
2024-05-13 20:21:44 +0300

    Format files

--- src/utilities/signInUtilities.ts
+++ src/utilities/signInUtilities.ts
@@ -12,15 +12,11 @@ const signInWithProvider = async (
   provider: AuthProviderType
 ): Promise<void> => {
   try {
-    // Attempting to sign in using a popup window with the specified provider
     const credential = await signInWithPopup(auth, provider);
 
-    // Retrieving additional user information from the authentication credential
     const userMeta: any = getAdditionalUserInfo(credential);
 
-    // Checking if the user is new or an existing user
     if (userMeta?.isNewUser) {
-      // If the user is new, set up their data in the Firebase Realtime Database
       set(ref(database, `users/${credential.user.uid}`), {
         name: credential.user.displayName,
         createdAt: serverTimestamp(),
@@ -28,10 +24,8 @@ const signInWithProvider = async (
       });
     }
 
-    // Alerting the user about successful login
     alert("You have successfully logged in.");
   } catch (error) {
-    // Handling any errors that occur during the authentication process
     console.log(error);
     alert("Something went wrong");
   }
