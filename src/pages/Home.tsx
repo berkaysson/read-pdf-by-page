a7b45e20d8acffc625c1520089fd769871b700ee
berkaysson <berkaysonel85@gmail.com>
2024-07-14 20:38:39 +0300

    add a loading modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { PDFDisplay } from "../components/main/displays/PDFDisplay";
 import PdfMenuBar from "../components/navigation/PdfMenuBar";
+import LoadingModal from "../components/main/displays/LoadingModal";
 
 export const Home: React.FC = () => {
   return (
     <div className="flex flex-col justify-center gap-8 align-middle">
+      <LoadingModal />
       <PdfMenuBar />
       <PDFDisplay />
     </div>
